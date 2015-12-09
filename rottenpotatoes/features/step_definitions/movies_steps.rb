84f229dad2ee921d29ae319e4ed1694f1276c2e5
Joaquin Morcate <jmorcate@gmail.com>
2015-12-09 15:59:33 +0000

    Changed the message Undefined step: "the following movies exist:" (Cucumber::Undefined) to "TODO (Cucumber::Pending) ./features/step_definitions/movies_steps.rb:3:in "/^the following movies exist:$/"

---
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -0,0 +1,12 @@
+Given(/^the following movies exist:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am in the Rottenpotatoes Home page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should view all the movies sorted by title$/) do
+  pending # express the regexp above with the code you wish you had
+end
