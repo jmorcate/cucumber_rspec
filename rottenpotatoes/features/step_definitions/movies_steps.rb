dfe6d8fa168147c540e2d6d166a8bde3af7651b2
Joaquin Morcate <jmorcate@gmail.com>
2015-12-10 13:23:00 +0000

    Cambiado error de: 'TODO (Cucumber::Pending)' a 'uninitialized constant Pelicula (NameError)'

--- rottenpotatoes/features/step_definitions/movies_steps.rb
+++ rottenpotatoes/features/step_definitions/movies_steps.rb
@@ -1,6 +1,9 @@
-Dadas(/^las siguientes películas:$/) do |table|
+Dadas(/^las siguientes películas:$/) do |tabla_peliculas|
   # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  # http://www.rubydoc.info/gems/cucumber/1.3.17/Cucumber/Ast/Table
+  tabla_peliculas.hashes.each do |pelicula|
+      Pelicula.create pelicula
+  end
 end
 
 Dadas(/^que estoy en la página inicial de RottenPotatoes$/) do
